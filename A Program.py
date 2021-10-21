42380c204dcb1a55cf7b86de2f5a0d9a8723907d
WHITEW0LF1 <92925567+WHITEW0LF1@users.noreply.github.com>
2021-10-21 17:37:49 +0530

    Create A Program.py

---
+++ A Program.py
@@ -0,0 +1,10 @@
+import random
+y = random.randint(1,10)
+x= input('Enter your name')
+print("Welcome" x)
+z = input('Choose a number between 1 and 10')
+if z = x:
+  print('Good Job you chose the correct number')
+else:
+  print("Sorry, you're wrong")
+  
