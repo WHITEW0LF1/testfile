85f354859c3d3858188f9558d539470e86e981a9
WHITEW0LF1 <92925567+WHITEW0LF1@users.noreply.github.com>
2021-11-12 15:33:30 +0530

    Update A Program.py

--- A Program.py
+++ A Program.py
@@ -1,8 +1,8 @@
 import random
-y = random.randint(1,10)
+y = random.randint(1,5)
 x= input('Enter your name')
 print("Welcome" x)
-z = input('Choose a number between 1 and 10')
+z = input('Choose a number between 1 and 5')
 if z = x:
   print('Good Job you chose the correct number')
 else:
